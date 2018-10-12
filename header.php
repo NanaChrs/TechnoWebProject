334daf3f32a2d9842d58b5b2ac75d272d915774d
Unknown <mathilde.christiaens@isen.yncrea.fr>
2018-10-12 16:53:01 +0200

    BDD Attempt

---
+++ header.php
@@ -0,0 +1,10 @@
+<header>
+	<center>
+		<ul>
+			<a href="index.php"><li>Home</li></a>
+			<a href="traitement_co.php"><li>Connexion ou Inscription</li></a>
+			<a href="CategShop.html"><li>Shopping par Catégorie</li></a>
+		    <a href="panier.html"><li>Panier<img src="panierIcon.png" height=15px class="panier"></li></a>
+		</ul>
+	</center>
+</header>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <title>Félindus - Home</title>
+     <link rel="stylesheet" href="wegotstyle.css"/>
+     <link href="https://fonts.googleapis.com/css?family=Cutive+Mono" rel="stylesheet">
+     <link rel="shortcut icon" href="panierIcon.png"/>
+	 <meta charset="utf-8"/>
+    
+
+</head>
+<body>
+	<?php 
+		include("header.php");
+		 
+
+
+	?>
+
+
+	
+
+</body>
+</html>
