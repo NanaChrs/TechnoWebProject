b157840093a655bcb2f92bec2c1bc5cba817c556
Unknown <mathilde.christiaens@isen.yncrea.fr>
2018-10-13 20:06:18 +0200

    Index like teacher want

--- index.php
+++ index.php
@@ -1,26 +1,43 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-    <title>Félindus - Home</title>
-     <link rel="stylesheet" href="wegotstyle.css"/>
-     <link href="https://fonts.googleapis.com/css?family=Cutive+Mono" rel="stylesheet">
-     <link rel="shortcut icon" href="panierIcon.png"/>
-	 <meta charset="utf-8"/>
-    
-
-</head>
-<body>
 
 
-	<?php 
-		include("header.php");
+<?php
+	session_start();
+	include("doctype.php");
+	include("header.php");
+	try {$bdd = new PDO('mysql:host=localhost;dbname=felindus;charset=utf8', 'root', '');}
+	    catch (Exception $e){die('Erreur : ' . $e->getMessage());}
+
+//TODO get page parameter ($_GET['page'] or $_POST['page']) and assign it into $page variable
+
+//if 'action/'.$page'.php' exists then include it (use file_exists($filename) function)
 
 
+//create one php file for each action to manage on the website
 
-	?>
+//TODO use 
+//             input params (included in $_GET or $_POST)
+//             $database variable (initialized in $database.php) 
+// to insert or update data into database
+// TODO insert the start html envelope (<html><head>....</head><body>
+
+// TODO using $page decide to include header.php
+
+//TODO add header display
+//TODO if 'view/'.$page'.php' exists then include it (use file_exists($filename) function)
+//           else include 'view/main.php' (it has to exist)
 
 
+//create one php file for each view to manage on the website (don't forget to create on main.php view)
+
+//TODO use 
+//             input params (included in $_GET or $_POST)
+//             $database variable (initialized in $database.php) 
+// to get data from database (if needed)
+
+// add view display possibly using data from database
+// TODO insert the end html envelope (</body></html>)
+
+?>
 	
 
 </body>
