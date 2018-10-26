8a7aa3f92512259e8dca903a297b52286e0df34c
JAROSSET Corentin <corentin.jarosset@isen.yncrea.fr>
2018-10-26 18:14:27 +0200

    panier avec SESSION et bouton supprimer qui fonctionnent

--- panier.php
+++ panier.php
@@ -1,16 +1,25 @@
 	<?php
-		if (isset($_POST['Id'])){
-			$_SESSION['panier'][] = array($_POST['Id'],$_POST['Nom'],$_POST['Prix'],$_POST['Quantite']);
+		if (isset($_GET['Id'])){
+			$_SESSION['panier'][count($_SESSION['panier'])] =array($_GET['Id'],$_GET['Nom'],$_GET['Prix'],$_GET['Quantite']);
 		}
-		if (isset($_SESSION['panier'])){
-			$tableau = array(array(" "," ",0,0));
+		if (empty($_SESSION['panier'])){
+			$tableau = array(array(" RIEN "," ",0,0));
 		}
 		else{
+			$temp = array();
+			for($l=0;$l<count($_SESSION['panier']);$l++){
+				if(!isset($_GET[$l])){
+					$temp[]=$_SESSION['panier'][$l];
+				}
+			}
+			unset($_SESSION['panier']);
+			$_SESSION['panier']=$temp;
 			$tableau = $_SESSION['panier'];
 		}
 		$NbrLigne=count($tableau)-1;
 		$NbrCol=3;
 		$Prixtoto=0;
+		//var_dump($_SESSION); fonction très utile ^^
 	?>
 
 
@@ -32,10 +41,10 @@
 				</tr></thread>
 				<tbody><?php for ($i=0; $i<=$NbrLigne; $i++) { ?><tr bgcolor=#aec5eb>
 				<?php for($j=0;$j<=$NbrCol;$j++) { ?>
-				<td><center><?php echo $tableau[$i][$j]; ?></center></td>
+				<td><center><?php echo ($tableau[$i][$j]); ?></center></td>
 				<?php } ?>
-				<td><center><?php $Prixtoto+=$tableau[$i][2]*$tableau[$i][3]; echo $tableau[$i][2]*$tableau[$i][3] ; ?></center></td>
-				<td><center><a href="index.php?page=panier" onclick="<?php unset($_SESSION['panier'][$i]); ?>"><input name="X" type="submit" value="X" class="boutonrond"/></a><center></td>  <!-- bouton supprimé à faire plus tard-->
+				<td><center><?php $Prixtoto+=$tableau[$i][2]*$tableau[$i][3];; echo ($tableau[$i][2]*$tableau[$i][3]) ; ?></center></td>
+				<td><center><form form="post"> <input name="page" value="panier" hidden><!--<a href="index.php?page=panier" >--><input  type="submit" name=<?php echo $i?> value="X" class="boutonrond"/><!--</a>--></form><center></td>  <!-- bouton supprimé à faire plus tard-->  
 				</tr><?php } ?></tbody>
 				<tbody><tr bgcolor=#edafb8>
 				<td colspan="7"><center>Prix Total : <?php echo $Prixtoto; ?></center></td>
