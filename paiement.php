d88890bd9579ff72a7c124ba40535548e3513a23
Antoine Fleury <antoine.fleury@isen.yncrea.fr>
2018-11-09 16:33:16 +0100

    look de payement arrangé

--- paiement.php
+++ paiement.php
@@ -12,57 +12,51 @@
      
 </head>
 <body>
-
-
-	
-		<div class="paiement">
-
-
 			<div class="col1">
-
 				<?php
+					//calcul du total du panier
 					$tableau = $_SESSION['panier'];
 					$NbrLigne=count($tableau)-1;
 					$Prixtoto=0;
 					for ($i=0; $i<=$NbrLigne; $i++) {
 							$Prixtoto+=$tableau[$i][2]*$tableau[$i][3];
 						}
-				?>			
-				<h1>Total Panier : <?php echo $Prixtoto; ?>€</h1>
+				?>
 
+				<h1>Total Panier : <?php echo $Prixtoto; ?>€</h1>
 				<a href="index.php?page=panier"><input type="button" name="modifier" class="btn" value="Modifier"></a>		
-				
 			</div>
 
-
 			<div class="col2">
-				<br><br>
 				<h1> Paiement</h1>
-					<form action="traitement_pay.php" method="post">
-					<p>Indiquez les informations correspondant à votre carte</p>
-
+					<form action="traitement_co.php" method="post">
+					<label for="choix1">Indiquez les informations correspondant à votre carte :</label>
 					<INPUT type="radio" name="choix1" value="1"> Visa
 					<INPUT type="radio" name="choix1" value="2"> Master Card
 					<INPUT type="radio" name="choix1" value="3"> CB
 					<INPUT type="radio" name="choix1" value="4"> Maestro
+					</br></br>
 
-					<label for="Numéro_carte">Numéro de carte :</label></br><input type="text" maxlength="16" name="Numéro_carte" format="NNNNNNNNNNNNNNNN" id="Numéro_carte" /></br>
-					<form>
-					  	<label for="Date_expiration">Date d'expiration :</label>
-					  	<input id="Date_expiration" type="month" name="Date_expiration"
-					  			min="2018-10">
-					</form>
-					<label for="code_sécurité">Code de sécurité :</label></br><input type="text" maxlength="3" name="code_sécurité" format="NNN" id="code_sécurité" /></br>
-					<p>J'accepte la conservation sécurisée de mes données bancaires</br>pour mes prochaines commandes :</p>
-					<FORM>
+					<label for="Numéro_carte">Numéro de carte :</label>
+					<input type="text" maxlength="16" name="Numéro_carte" format="NNNNNNNNNNNNNNNN" id="Numéro_carte" />
+					</br></br>
+
+					<label for="Date_expiration">Date d'expiration :</label>
+					<input id="Date_expiration" type="month" name="Date_expiration" min="2018-10">
+					</br></br>
+
+					<label for="code_sécurité">Code de sécurité :</label>
+					<input type="text" maxlength="3" name="code_sécurité" format="NNN" id="code_sécurité" />
+					</br></br>
+
+					<label for="choix2">J'accepte la conservation sécuriséede</br>mes données bancaires pour mes prochaines commandes :</label>
 					<INPUT type="radio" name="choix2" value="5"> Oui
 					<INPUT type="radio" name="choix2" value="6"> Non
-					</FORM>
+					</br></br>
 					
 				<a href="index.php?page=merci"><input name="payer" type="submit" id="payer" value="Payer" class="btn"/></a>
 				</form>
 			</div>
-		</div>
 </body>
 
 </html>
