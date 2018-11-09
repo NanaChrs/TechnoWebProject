212bcc294030a9e0181c29407dab62f6f8ef1d85
Story DeWeese <sdeweese@scu.edu>
2018-11-09 18:06:12 +0100

    put all style into one css
    I left both the wegotstyle.css and  wegotstyle(Axel).css, so we can revert back if we need to!

--- doctype.php
+++ doctype.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
     <title>Félindus - Home</title>
-     <link rel="stylesheet" href="wegotstyle.css"/>
+     <link rel="stylesheet" href="story.css"/>
      <link href="https://fonts.googleapis.com/css?family=Cutive+Mono" rel="stylesheet">
      <link rel="shortcut icon" href="Images/panierIcon.png"/>
 	 <meta charset="utf-8"/>
