485dd5092c6703e707653b0bbc877dc2eff22a0e
Antoine Fleury <antoine.fleury@isen.yncrea.fr>
2018-11-09 14:25:22 +0100

    modif page paiement
    -suppression du session[panier] après paiement
    -ajout de l'idClient dans la bdd commande

--- merci.php
+++ merci.php
@@ -19,23 +19,25 @@
 	
 <?php
 		
-		//se connecter à la BDD
 
-		//$tableau = array(array(1,"chaton",100,1),array(2,"chien",50,1));
-		$tableau = $_SESSION['panier'];
+		//$tableau = array(array(1,"chaton",100,1),array(2,"chien",50,1)); //test sans le $_SESSION
+
+		$tableauPanier = $_SESSION['panier']; //on récupère les infos
+		$tableauClient = $_SESSION['client']; //on récupère les infos clients
+		$idClient = $tableauClient[0]; //idClient dans une variable
 
 		// On ajoute ce qui a été acheté dans la table 'commande'
-		$NbrLigne=count($tableau)-1;
+		$NbrLigne=count($tableauPanier)-1;
 		for ($j=0; $j<=$NbrLigne; $j++) {
-			$bdd->exec('INSERT INTO commande(IdProduit, Nom, Qte, IdClient) VALUES('.$tableau[$j][0].', "'.$tableau[$j][1].'", '.$tableau[$j][3].',2)'); //il reste à modifier d'IdClient
+			$bdd->exec('INSERT INTO commande(IdProduit, Nom, Qte, IdClient) VALUES('.$tableauPanier[$j][0].', "'.$tableauPanier[$j][1].'", '.$tableauPanier[$j][3].', '.$idClient.')');
 		}
 
 		// On retire ce qui a été acheté de la table 'produit'
 		for ($j=0; $j<=$NbrLigne; $j++) {
-			$bdd->exec('UPDATE produit SET QteStock = QteStock -'.$tableau[$j][3].' WHERE IdProduit = '.$tableau[$j][0].'');
+			$bdd->exec('UPDATE produit SET QteStock = QteStock -'.$tableauPanier[$j][3].' WHERE IdProduit = '.$tableauPanier[$j][0].'');
 		}
 
-		echo '</div>';
+		unset($_SESSION['panier']); //supprime le panier une fois qu'il a été ajouté dans la bdd commande
 	?>
 
 
